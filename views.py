78fc3e8bfe4cdd39f42a0e48d842ba7d485078c7
Lucas <lucas.borggreve@gmail.com>
2026-08-18 12:48:48 -0300

    removendo argumento load_data

--- views.py
+++ views.py
@@ -5,14 +5,14 @@ def index():
     note_template = load_template('components/note.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
-        for dados in load_data('notes.json')
+        for dados in load_data()
     ]
     notes = '\n'.join(notes_li)
 
     return load_template('index.html').format(notes=notes)
 
 def submit(titulo,detalhes):
-    notes = load_data('notes.json')
+    notes = load_data()
     note = {"titulo" : titulo, "detalhes": detalhes}
     notes.append(note)
     with open(f'static/data/notes.json', 'w') as arquivo:
