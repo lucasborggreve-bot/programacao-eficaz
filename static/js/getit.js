588323d339a5ebb5bcfb69491742d96247e9b3c7
Lucas <lucas.borggreve@gmail.com>
2026-08-18 12:41:54 -0300

    criacao tabela e banco de dados

--- static/js/getit.js
+++ static/js/getit.js
@@ -1,4 +1,4 @@
-// Mantém o foco no primeiro campo após o carregamento da página.
+
 document.addEventListener('DOMContentLoaded', () => {
     const titleField = document.querySelector('#titulo');
     if (titleField) {
--- utils.py
+++ utils.py
@@ -1,8 +1,27 @@
-import json
+import sqlite3
 
-def load_data(notes):
-    with open(f'static/data/{notes}', 'r', encoding='utf-8') as arquivo:
-        return json.load(arquivo)
+def criar_tabela():
+    conexao = sqlite3.connect("banco.db")
+    cursor = conexao.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS note (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT
+);""")
+    conexao.commit()
+    conexao.close()
+
+
+
+def load_data():
+    conexao = sqlite3.connect("banco.db")
+    cursor = conexao.cursor()
+    execucao = cursor.execute("SELECT * FROM note")
+    recuperacao = execucao.fetchall()
+    conexao.close()
+    return recuperacao
+
+
 
 def load_template(template):
     with open(f'static/templates/{template}', 'r', encoding='utf-8') as arquivo:
