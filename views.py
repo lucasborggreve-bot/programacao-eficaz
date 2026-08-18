211c0cd93b93f43d1d54e1b2b292a6e171f5f4f6
Lucas <lucas.borggreve@gmail.com>
2026-08-18 12:59:17 -0300

    arrumando erro de tabela

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ def index():
     note_template = load_template('components/note.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
-        for dados in load_data()
+        for dados in load_data('notes.json')
     ]
     notes = '\n'.join(notes_li)
 
